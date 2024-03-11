d3e935392871b1d9fbb58892feba2dd65f446fde
MrNineMan <bearth2501@yahoo.com>
2024-03-11 06:10:20 +0200

    Initial Commit

--- src/main/java/draft/dev/Main.java
+++ src/main/java/draft/dev/Main.java
@@ -2,17 +2,20 @@ package draft.dev;
 
 //Assembly AI Imports
 import com.assemblyai.api.AssemblyAI;
-import com.assemblyai.api.resources.transcripts.types.*;
+import com.assemblyai.api.resources.transcripts.types.Transcript;
+import com.assemblyai.api.resources.lemur.requests.LemurTaskParams;
 
-import java.awt.*;
+
+import java.awt.Desktop;
 import java.io.File;
 import java.io.IOException;
 import java.io.FileWriter;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[]args) {
-        AssemblyAI client = AssemblyAI.builder().apiKey("ec855817c8824b0d8019eb4e6f80cb93").build(); //Remember to remove API Key
+        AssemblyAI client = AssemblyAI.builder().apiKey("62053e25eb0b4512a79e1dae34744f83").build(); //Remember to remove API Key
 
         try {
             File audioFile = new File("./audio/apple_public_earnings_call.mp3");
@@ -21,7 +24,9 @@ public class Main {
             outputFile.createNewFile();
             FileWriter fileWriter = new FileWriter(outputFile);
             //Transcribe audio file
-            Transcript transcript = client.transcripts().transcribe(audioFile);
+            Transcript transcript = client.transcripts().transcribe(outputFile);
+
+
             //Checks to see if transcription was successful
             if (transcript.getStatus().toString().contentEquals("error")) {
                 System.err.println(transcript.getError());
@@ -32,6 +37,18 @@ public class Main {
                 //Opens output file
                 Desktop desktop = Desktop.getDesktop();
                 desktop.open(outputFile);
+
+/*                //Generate prompt
+                var params = LemurTaskParams.builder()
+                        .prompt("Extract insights from the transcript.").transcriptIds(List.of(transcript.getId()))
+                        .build();
+                //Execute prompt
+                var result = client.lemur().task(params);
+
+                //Print response
+                System.out.println(result.getResponse());*/
+
+
             }
 
         } catch (IOException ioException) {
---
+++ src/main/java/draft/dev/Transcription Source Code/Main.java
@@ -0,0 +1,62 @@
+package draft.dev;
+//Begin Assembly AI Imports
+
+import com.assemblyai.api.AssemblyAI;
+
+import com.assemblyai.api.resources.transcripts.types.Transcript;
+
+//End Assembly AI Imports
+
+import java.awt.Desktop;
+
+import java.io.File;
+
+import java.io.IOException;
+
+import java.io.FileWriter;
+
+public class Main {
+
+	public static void main(String[]args) { 
+	
+		AssemblyAI client = AssemblyAI.builder().apiKey("62053e25eb0b4512a79e1dae34744f83").build();
+		try {
+			//Apple public earnings call audio file   
+			File audioFile = new File("./audio/apple_public_earnings_call.mp3");
+
+			//Create a new output file and filewriter for the transcription
+
+			File outputFile = new File("output.txt");
+
+			outputFile.createNewFile();
+
+			FileWriter fileWriter = new FileWriter(outputFile);
+			
+			Transcript transcript = client.transcripts().transcribe(audioFile);
+			
+			if (transcript.getStatus().toString().contentEquals("error")) {
+				System.err.println(transcript.getError());
+			} else {
+
+				//Write transcript to output file
+
+				fileWriter.write(String.valueOf(transcript.getText()));
+
+				fileWriter.close();
+
+				//Opens output file
+
+				Desktop desktop = Desktop.getDesktop();
+
+				desktop.open(outputFile);
+
+			} 
+			
+			
+		} catch (IOException ioException) {
+			ioException.printStackTrace();
+		}	
+		
+	}
+
+}
